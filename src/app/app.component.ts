cfcd9773c5b071e475b11b0860caa427d4d5a20e
fina <finawei@gmail.com>
2017-08-18 17:11:44 +0200

    Add Filling dDates functions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+public checkinDate: Date;
+public checkoutDate: Date;
+public bookingDates: any[];
+
+constructor(private http: HttpClient) {}
+
+// post checkin and checkout date to back end
+public save() {
+console.log(this.checkinDate);
+const body = {checkinDate: this.checkinDate, checkoutDate: this.checkoutDate};
+console.log(body);
+// send post request
+this.http.post('http://localhost:8080/dateregistration', body).subscribe((data) => {
+  alert('Reservation saved!');
+});
+}
+
+
+// get checkin and checkout overview
+public overview() {
+this.http.get('http://localhost:8080/dateregistration').subscribe((data: any) => {
+console.log(data);
+this.bookingDates = data;
+  });
+}
+
+// clear all bookings
+public clear() {
+this.http.delete('http://localhost:8080/dateregistration').subscribe( () => {
+this.bookingDates = null;
+  });
+}
+
 }
