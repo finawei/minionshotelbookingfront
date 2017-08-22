a230c8a910afee26d1972950205aa4f688297e30
fina <finawei@gmail.com>
2017-08-22 12:35:47 +0200

    materialized

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,15 +1,26 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MdButtonModule, MdCheckboxModule } from '@angular/material';
+import { MdButtonModule, MdCheckboxModule,
+    MdDatepickerModule, MdInputModule, MdNativeDateModule, MdIconModule } from '@angular/material';
+
 
 @NgModule({
     imports: [
         CommonModule,
         MdButtonModule,
-        MdCheckboxModule
+        MdCheckboxModule,
+        MdDatepickerModule,
+        MdInputModule,
+        MdNativeDateModule,
+        MdIconModule
     ],
     exports: [
-        MdButtonModule, MdCheckboxModule
+        MdButtonModule,
+        MdCheckboxModule,
+        MdDatepickerModule,
+        MdInputModule,
+        MdNativeDateModule,
+        MdIconModule
     ],
     declarations: []
 })
